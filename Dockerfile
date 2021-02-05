817e33ead3fde9b325518b757b5cbdd381d51b6d
Mihaly Arvai <mihaly.arvai@mirumagency.com>
2021-02-05 14:51:58 +0100

    docker support

---
+++ Dockerfile
@@ -0,0 +1,69 @@
+﻿# escape=`
+
+ ARG BASE_IMAGE
+ ARG BUILD_IMAGE
+
+ FROM ${BUILD_IMAGE} AS prep
+
+# # Gather only artifacts necessary for NuGet restore, retaining directory structure
+ COPY *.sln nuget.config \nuget\
+ COPY src\ \temp\
+ RUN Invoke-Expression 'robocopy C:\temp C:\nuget\src /s /ndl /njh /njs *.csproj'
+
+FROM ${BUILD_IMAGE} AS builder
+
+ ARG BUILD_CONFIGURATION
+
+ SHELL ["powershell", "-Command", "$ErrorActionPreference = 'Stop'; $ProgressPreference = 'SilentlyContinue';"]
+
+ RUN Invoke-WebRequest -OutFile nodejs.zip -UseBasicParsing "https://nodejs.org/dist/v8.17.0/node-v8.17.0-win-x64.zip"; `
+     Expand-Archive nodejs.zip -DestinationPath C:\; `
+     Rename-Item "C:\\node-v8.17.0-win-x64" c:\nodejs
+
+ RUN SETX PATH C:\nodejs
+ RUN npm config set registry https://registry.npmjs.org/
+
+
+
+# # Create an empty working directory
+ WORKDIR C:\build
+
+# # Copy prepped NuGet artifacts, and restore as distinct layer to take better advantage of caching
+ COPY --from=prep .\nuget .\
+ RUN nuget restore
+
+# # Copy remaining source code
+ COPY src\ .\src\
+
+# # Copy transforms, retaining directory structure
+ RUN Invoke-Expression 'robocopy C:\build\src C:\out\transforms /s /ndl /njh /njs *.xdt'
+
+# # Build website with file publish
+ #RUN msbuild .\src\website\website\helixbase.Website.csproj /p:DeployHelix=true /p:PublishProfile=DockerBuild.pubxml /t:Restore,Build /p:Configuration=Debug
+RUN msbuild .\src\Healthcheck.Service\Healthcheck.Service.csproj  /t:Restore,Build /p:PublishProfile=DockerBuild /p:DeployOnBuild=True
+# # Build XConnect with file publish
+# RUN msbuild .\src\DockerExamples.XConnect\DockerExamples.XConnect.csproj /p:Configuration=$env:BUILD_CONFIGURATION /p:DeployOnBuild=True /p:DeployDefaultTarget=WebPublish /p:WebPublishMethod=FileSystem /p:PublishUrl=C:\out\xconnect
+
+# Create build directory for speak
+WORKDIR C:\build_speak
+COPY src\Healthcheck.Client\ .\
+RUN npm install
+RUN npm run deploy
+
+
+
+ FROM ${BASE_IMAGE}
+
+ 
+ WORKDIR C:\artifacts
+
+ # Copy final build artifacts
+ COPY --from=builder C:\out\website .\website\
+ ## COPY --from=builder C:\build .\build\
+ COPY --from=builder C:\out\transforms .\transforms\
+
+ COPY --from=builder C:\out\speakapp .\speakapp\
+# COPY --from=builder C:\out\xconnect .\xconnect\
+
+
+
---
+++ src/Healthcheck.Service.Remote/AssemblyResolver.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.Pipelines;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Healthcheck.Service.Remote
+{
+    public class AssemblyResolver : IServicesConfigurator
+    {
+        public void Process(PipelineArgs args)
+        {
+            var assemblyNamespace = Assembly.GetExecutingAssembly().GetName().Name;
+            var assemblyName = $"{assemblyNamespace}.VersionSpecific";
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+            {
+                if (eventArgs.Name.Contains(","))
+                {
+                    if (eventArgs.Name.Substring(0, eventArgs.Name.IndexOf(",", StringComparison.Ordinal)) != assemblyName)
+                        return null;
+                }
+                else
+                {
+                    if (string.Compare(eventArgs.Name, assemblyName, StringComparison.OrdinalIgnoreCase) != 0)
+                        return null;
+                }
+
+                return LoadVersionAssembly(assemblyNamespace, assemblyName);
+            };
+
+            LoadVersionAssembly(assemblyNamespace, assemblyName);
+        }
+
+        private static Assembly LoadVersionAssembly(string assemblyNamespace, string assemblyName)
+        {
+            Sitecore.Diagnostics.Log.Info($"Assembly Resolve: {assemblyName}, {assemblyNamespace}", new object());
+            return null;
+        }
+
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            var assemblyNamespace = Assembly.GetExecutingAssembly().GetName().Name;
+            var assemblyName = $"{assemblyNamespace}.VersionSpecific";
+
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, eventArgs) =>
+            {
+                if (eventArgs.Name.Contains(","))
+                {
+                    if (eventArgs.Name.Substring(0, eventArgs.Name.IndexOf(",", StringComparison.Ordinal)) != assemblyName)
+                        return null;
+                }
+                else
+                {
+                    if (string.Compare(eventArgs.Name, assemblyName, StringComparison.OrdinalIgnoreCase) != 0)
+                        return null;
+                }
+
+                return LoadVersionAssembly(assemblyNamespace, assemblyName);
+            };
+
+            LoadVersionAssembly(assemblyNamespace, assemblyName);
+        }
+    }
+}
