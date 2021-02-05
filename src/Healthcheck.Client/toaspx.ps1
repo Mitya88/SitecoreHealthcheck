ac5f22c839424f74addefc8a29f2c97264940f78
Mihaly Arvai <mihaly.arvai@mirumagency.com>
2021-02-05 17:52:48 +0100

    adding react

--- src/Healthcheck.Client/toaspx.ps1
+++ src/Healthcheck.Client/toaspx.ps1
@@ -1,5 +1,5 @@
 ﻿
 
 
-((Get-Content -path .\build\index.html -Raw) -replace '<!DOCTYPE html>','<%@ Page AutoEventWireup=\"true\" Inherits=\"Sitecore.Speak.Integration.Spa.SecureSpaPage\" Language=\"C#\" %><!DOCTYPE html>') | Set-Content -Path .\build\index.aspx
+((Get-Content -path .\build\index.html -Raw) -replace '<!DOCTYPE html>','<%@ Page AutoEventWireup="true" Inherits="Sitecore.Speak.Integration.Spa.SecureSpaPage" Language="C#" %><!DOCTYPE html>') | Set-Content -Path .\build\index.aspx
 del .\build\index.html
